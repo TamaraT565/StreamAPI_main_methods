17ea8db4df8cff9d441c14a850c38ee603af1f20
Tamara Tatarintseva <tta.nemo@gmail.com>
2023-02-15 10:53:04 +0300

    made edits

--- src/Main.java
+++ src/Main.java
@@ -17,17 +17,16 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
-        long minor_people = persons.stream()
+        long minorPeople = persons.stream()
                 .filter(person -> person.getAge() < 18)
                 .count();
-        System.out.println("Количество несовершеннолетних жителей равно - " + minor_people);
+        System.out.println("Количество несовершеннолетних жителей равно - " + minorPeople);
 
         System.out.println("----------------------------------------------");
        List<String> recruits = persons.stream()
                .filter(person -> person.getSex() == Sex.MAN)
-               .filter(person -> person.getAge() >= 18)
-               .filter(person ->person.getAge() <= 27)
-               .map(person -> person.getFamily().toString())
+               .filter(person -> person.getAge() >= 18 && person.getAge() <= 27)
+               .map(person -> person.getFamily())
                .collect(Collectors.toList());
         System.out.println("Список фамилий мужчин призывников: " + recruits);
 
@@ -35,10 +34,10 @@ public class Main {
 
         List<String> workers = persons.stream()
                 .filter(person -> person.getAge()>= 18)
-                .filter(Person ->Person.getSex() == Sex.WOMAN? Person.getAge() < 60: Person.getAge() < 65)
+                .filter(person ->person.getSex() == Sex.WOMAN? person.getAge() < 60: person.getAge() < 65)
                 .filter(person -> person.getEducation() == Education.HIGHER)
                 .sorted(Comparator.comparing(Person::getFamily))
-                .map(person -> person.getFamily().toString())
+                .map(person -> person.getFamily())
                 .collect(Collectors.toList());
         System.out.println("Список работоспособного населения с высшим образованием" + workers);
     }
